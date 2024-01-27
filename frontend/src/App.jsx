e4ee5875359196a49928ec9af6a5d480e80d0572
PranayKalyanam <kalyanampranay45@gmail.com>
2024-01-27 12:39:32 +0530

    updated app.js file and package.json file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { AuthProvider } from './Context/AuthContext'
+import Login from './Components/Auth/Login'
+import Register from './Components/Auth/Register'
+import Home from './Pages/Home'
+import CreateProject from './Pages/CreateProject'
+
+import Universities from './Pages/Universities'
+import Projects from './Pages/Projects'
+import UniversityPage from './Components/UniversityComponents/UniversityPage'
+import ProjectPage from "./Components/ProjectComponents/Project"
+import Profile from './Pages/Profile'
+
+import AdminLogin from './Admin/AdminLogin'
+import AdminRegistrationForm from "./Admin/AdminRegister"
+import { AdminAuthProvider } from './Context/AdminAuthContext'
+import Admin from "./Admin/Admin"
+import AddProfessor from './Admin/AddProfessor'
+import ProfessorList from './Admin/ProfessorList'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <AuthProvider>
+
+          <AdminAuthProvider>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/universities" element={<Universities />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/create-project" element={<CreateProject />} />
+              <Route path="/profile" element={<Profile />} />
+
+              {/* Individual Pages */}
+              <Route path="/universities/:universityId" element={<UniversityPage />} />
+              <Route path="/project/:projectId" element={<ProjectPage />} />
+
+              {/*Auth Routes */}
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+
+              {/* Admin login              */}
+              <Route path="/admin/login" element={<AdminLogin />} />
+              <Route path="/admin/register" element={< AdminRegistrationForm />} />
+
+              {/* Admin Dashboard */}
+              <Route path="/admin" element={< Admin />} />
+              <Route path="/admin/add-professor" element={< AddProfessor />} />
+              <Route path="/admin/professor-list" element={< ProfessorList />} />
+
+
+
+
+
+
+            </Routes>
+          </AdminAuthProvider>
+
+        </AuthProvider>
+
+      </BrowserRouter>
+
+
+
+
     </>
+
+
   )
 }
 
