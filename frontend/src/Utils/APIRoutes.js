e3a9610aa3d87de0e7bbb3a08b27e108b50ef1cb
Govinda Varma <98372421+govindvarma1@users.noreply.github.com>
2024-01-31 18:12:10 +0530

    Update APIRoutes.js

--- frontend/src/Utils/APIRoutes.js
+++ frontend/src/Utils/APIRoutes.js
@@ -7,20 +7,20 @@ const API_ROUTE = "http://localhost:5000";
 export const StudentLoginRoute = `${API_ROUTE}/api/student/login`; //Student Login
 export const StudentRegister = `${API_ROUTE}/api/student/register`; //Student Register
 export const StudentPendingProjects = `${API_ROUTE}/api/student/`; //Student Pending Project
-export const StudentTokenVerifyRoute = `${API_ROUTE}/api/auth/verify-token`; //Student Token Verify
+export const StudentTokenVerifyRoute = `${API_ROUTE}/api/student/verify-token`; //Student Token Verify
 
 //Professor Routes
-export const ProfessorTokenVerifyRoute = `${API_ROUTE}/api/auth/verify-token`;
+export const ProfessorTokenVerifyRoute = `${API_ROUTE}/api/professor/verify-token`; //Student Token Verify
 export const ProfessorLoginRoute = `${API_ROUTE}/api/professor/login`;
 export const ProfessorRegister = `${API_ROUTE}/api/professor/register`;
 export const GetPendingProjects = `${API_ROUTE}/api/professor/all-pending-projects`;
 export const GetApprovedProjects = `${API_ROUTE}/api/professor/all-approved-projects`;
 
 //Admin Routes
+export const AdminTokenVerifyRoute = `${API_ROUTE}/api/admin/verify-token`; //Student Token Verify
 export const AdminRegisterRoute = `${API_ROUTE}/api/admin/register`;
 export const AdminLoginRoute = `${API_ROUTE}/api/admin/login`;
 export const AddProfessorRoute = `${API_ROUTE}/api/admin/add-professor`;
-export const AdminTokenVerifyRoute = `${API_ROUTE}/api/auth/verify-token`;
 
 //Project Routes
 export const ProjectDetailsRoute = `${API_ROUTE}/api/project/`;
